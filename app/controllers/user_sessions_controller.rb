c63b76ecf516672f1000dfb64618c14a686160ea
BabaShun <you@example.com>
2016-10-08 03:22:16 +0900

    bag fix

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:users, notice, 'login successful!!!')
+      redirect_back_or_to(:users, notice: 'login successful!!!')
     else
       flash.now[:alert] = 'login failed'
       render action: 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users
+  resources :user_sessions
   resources :users
 
   get 'login' => 'user_sessions#new'
