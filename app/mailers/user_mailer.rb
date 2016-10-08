9789d291cfe0c382e87cf0e871f451bea8b34ec8
BabaShun <you@example.com>
2016-10-08 15:10:00 +0900

    bug fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,7 +16,7 @@ class UserMailer < ApplicationMailer
 
   def reset_password_email(user)
     @user = User.find user.id
-    @url = "http://localhost:3000" + edit_password_reset_path(@user.reset_password_token)
+    @url = edit_password_reset_url(@user.reset_password_token)
     mail(:to => user.email,  :subject => "Your password has been reset")
   end
 end
